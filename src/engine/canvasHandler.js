460c9e5a10ef6e72c5909b054db16c71daf7bfc0
nenikitov <nenikitov@mail.ru>
2021-09-13 22:52:28 -0400

    Documented and tested canvas handler

--- src/engine/canvasHandler.js
+++ src/engine/canvasHandler.js
@@ -1,11 +1,23 @@
 export class CanvasHandler {
     //#region Fields
+    /** Canvas object */
     #canvas;
+    /** 2d drawing context of the canvas */
     #ctx;
+    /** Aspect ration (width / height) */
     #ratio;
+    /** Rendering resolution multiplier */
     #resScale;
     //#endregion
 
+
+    //#region Constructors
+    /**
+     * Init the canvas handler
+     * @param {string} canvasId String id of the canvas object to render the game
+     * @param {number} aspectRatio Aspect ratio (width / height)
+     * @param {number} resolutionScale Multiplier for rendering resolution
+     */
     constructor(canvasId, aspectRatio, resolutionScale) {
         this.#canvas = document.querySelector("#" + canvasId);
         this.#ctx = this.#canvas.getContext('2d');
@@ -15,23 +27,29 @@ export class CanvasHandler {
 
         this.adjustSize();
     }
+    //#endregion
 
+
+    //#region Size
+    /**
+     * Update the aspect ratio of the canvas
+     * @param {number} aspectRatio Aspect ratio (width / height)
+     */
     setAspectRatio(aspectRatio) {
         this.#ratio = aspectRatio;
         this.clear();
     }
-
+    /**
+     * Update the rendering resolution
+     * @param {number} resolutionScale Multiplier for rendering resolution
+     */
     setResolutionScale(resolutionScale) {
         this.#resScale = resolutionScale;
         this.clear();
     }
-
-
-    clear() {
-        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
-    }
-
-    
+    /**
+     * Automatically adjust the size to the bigest rectangle of current aspect ratio
+     */
     adjustSize() {
         const windowWidth = window.innerWidth;
         const windowHeight = window.innerHeight;
@@ -48,7 +66,6 @@ export class CanvasHandler {
             this.#setDisplayResolution(windowWidth, targetHeight)
         }
     }
-
     #setRenderResolution(width, height, useResolutionScale = true) {
         const scale = useResolutionScale ? this.#resScale : 1;
         this.#canvas.width = width * scale;
@@ -58,4 +75,22 @@ export class CanvasHandler {
         this.#canvas.style.width = `${width}px`;
         this.#canvas.style.height = `${height}px`;
     }
+    //#endregion
+
+    //#region Drawing
+    /**
+     * Clear the rendering surface
+     */
+    clear() {
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+    }
+    drawTest() {
+        this.#ctx.fillStyle = 'green';
+        this.#ctx.fillRect(20, 20, 150, 100);
+    }
+    drawTest2() {
+        this.#ctx.fillStyle = 'red';
+        this.#ctx.fillRect(40, 40, 150, 100);
+    }
+    //#endregion
 }
--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,3 +1,7 @@
 import { CanvasHandler } from './canvasHandler.js'
 
-let canvasHandler = new CanvasHandler('renderSurface', 16/9, 0.5);
+let canvasHandler = new CanvasHandler('renderSurface', 16/9, 1);
+
+canvasHandler.drawTest();
+canvasHandler.clear();
+canvasHandler.drawTest2();

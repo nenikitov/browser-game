183847cc244980626a91ed4d3631287507f24524
nenikitov <nenikitov@mail.ru>
2021-09-22 19:06:28 -0400

    Implemented movement to test

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -2,18 +2,22 @@ import { CanvasHandler } from './rendering/canvas-handler.js'
 import { Vector2d } from './utils/vector.js';
 import { AssetHandler } from './loading/asset-handler.js';
 import { MouseListener } from './input/listeners/mouse-listener.js';
+import { KeyboardListener } from './input/listeners/keyboard-listener.js';
 
 const mouseListener = new MouseListener();
+const keyboardListener = new KeyboardListener();
 const canvasHandler = new CanvasHandler('renderSurface', 16/9, 1);
 
 const image = AssetHandler.get('image', './res/textures/test_char.png').asset();
-const pos = new Vector2d(0, 0);
+const pos = new Vector2d(128, 128);
 const size = new Vector2d(128, 128);
 
 window.requestAnimationFrame(tick);
 
 function tick() {
-    pos.copy(mouseListener.pos_);
+    const right = keyboardListener.isPressed('KeyD') - keyboardListener.isPressed('KeyA');
+
+    pos.add(new Vector2d(right, 0));
     canvasHandler.clear();
     canvasHandler.drawImage(image, pos, size);
 

26afd6b342f8c774aa404d3575b35966f5d85faa
nenikitov <nenikitov@mail.ru>
2021-09-29 20:18:15 -0400

    Testing input system

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,10 +1,21 @@
 import { CanvasHandler } from './rendering/canvas-handler.js'
 import { Vector2d } from './utils/vector.js';
 import { AssetHandler } from './loading/asset-handler.js';
-import { MouseListener } from './input/listeners/mouse-listener.js';
 import { KeyboardListener } from './input/listeners/keyboard-listener.js';
+import { GameAction, KeyBind } from './input/input-bind.js';
 
-const mouseListener = new MouseListener();
+const moveRightBind = new KeyBind ('KeyD', 1.0);
+const moveRightBindA = new KeyBind ('ArrowRight', 1.0);
+const moveLeftBind = new KeyBind ('KeyA', -1.0);
+const moveRightAction = new GameAction(false, moveRightBind, moveLeftBind, moveRightBindA)
+
+
+
+const moveUpBind = new KeyBind ('KeyW', 1.0);
+const moveDownBind = new KeyBind ('KeyS', -1.0);
+const moveUpAction = new GameAction(true, moveUpBind, moveDownBind);
+
+
 const keyboardListener = new KeyboardListener();
 const canvasHandler = new CanvasHandler('renderSurface', 16/9, 1);
 
@@ -14,10 +25,13 @@ const size = new Vector2d(128, 128);
 
 window.requestAnimationFrame(tick);
 
-function tick() {
-    const right = keyboardListener.isPressed('KeyD') - keyboardListener.isPressed('KeyA');
 
-    pos.add(new Vector2d(right, 0));
+
+function tick() {
+    const right = moveRightAction.value(keyboardListener);
+    const up = moveUpAction.value(keyboardListener);
+
+    pos.add(new Vector2d(right, -up));
     canvasHandler.clear();
     canvasHandler.drawImage(image, pos, size);
 
